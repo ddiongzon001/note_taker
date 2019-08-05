4cf2254aa46858fd4e58e0d9d724ba03384f6b41
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-05 18:15:17 -0400

    added the get route to display all information from the database to that api route of see all notes

--- server.js
+++ server.js
@@ -5,9 +5,30 @@ const path = require("path");
 
 // set up the express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // set up the express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
-app.use(express.json);
+app.use(express.json());
 
+// ROUTES
+// API routes
+
+// * Retrieving all notes from the database and returning them to the user as JSON.
+app.get("/api/see_notes", function(req, res){
+    let queryString = "SELECT * FROM notes";
+    connection.query(queryString, function(err, data){
+        if (err) throw err;
+        res.json(data);
+    })
+});
+
+// * Saving a new note to the database using the data passed on `req.body`.
+
+// * Deleting a note from the database using `req.params.id`.
+
+// LISTENER
+app.listen(PORT, function() {
+    console.log(`App listening on http://localhost:${PORT}`);
+  });
+  
