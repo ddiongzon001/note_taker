3024f3d894502af02670fd9ed9a47120bc758070
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-06 20:24:11 -0400

    changed the initial notes on the seeds.sql and then also added the initial display of notes to the javascript

--- public/assets/javascript/notes.js
+++ public/assets/javascript/notes.js
@@ -1,13 +1,55 @@
-
-
 // Initial display of notes to the page
-function displayNotes(){
+function displayNotes() {
+
+    $(".list-group").empty();
     // call to get the data
-    $.get("/api/see_notes", function(notesData){
+    $.get("/api/see_notes", function (notesData) {
         console.log(notesData);
+
+        for (var i = 0; i < notesData.length; i++) {
+            var noteDisplay = $("<a>");
+            noteDisplay.addClass('list-group-item list-group-item-action noteList');
+            noteDisplay.attr("data-id", notesData[i].id);
+            noteDisplay.attr("data-title", notesData[i].title);
+            noteDisplay.attr("data-body", notesData[i].body);
+            noteDisplay.text(notesData[i].title);
+            $(".list-group").prepend(noteDisplay);
+        }
     })
 
-
 }
+
+// saving a note to the database
+$("#new").on("click", function (event) {
+    event.preventDefault();
+
+    let newNote = {
+        title: $(".bunny-note-title").val().trim(),
+        body: $(".bunny-note-body").val().trim()
+    }
+
+    //clear the input lines
+    $(".bunny-note-title").val("");
+    $(".bunny-note-body").val("");
+
+    $.post("/api/save_note", newNote, function (data) {
+        console.log(data);
+    })
+
+    location.reload();
+})
+
+// displaying note on the main section
+$(".noteList").on("click", function (event) {
+    event.preventDefault();
+    console.log("this was clicked");
+    let currentTitle = $(this).attr('data-title');
+    let currentBody = $(this).attr('data-body');
+    console.log(currentTitle);
+    console.log(currentBody);
+    $(".bunny-note-title").val(currentTitle);
+    $(".bunny-note-body").val(currentBody);
+
+})
 
 displayNotes();
--- seeds.sql
+++ seeds.sql
@@ -2,8 +2,8 @@ USE notetaker_db;
 
 INSERT INTO notes (title, body)
 VALUES
-  ("Note Taker App", "Complete this homework by Tuesday, August 6, 2019 11:59 PM"),
-  ("READ THE DOCS", "Read documentation about... Node, Express, MySQL, etc."),
-  ("Mark myself present in Bootcampspot", "Remember to mark myself present every Monday-Friday at 10am"),
-  ("Commit more", "Get those commits up. It shows the process and progress.")
+  ("Feed Bunny", "Feed rabbits pellets and hay"),
+  ("Give Bunny Water", "Remember to always give your bunny a fresh bowl of water"),
+  ("Comb your bunny", "Every weekend comb your bunny"),
+  ("Clean your bunny's cage", "Every week, clean your bunny's cage")
   -- ADD MORE
